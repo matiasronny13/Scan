9ebae937e62807fe748b65f88196057ff699a79e
Administrator <Administrator@WIN-MEE7SG0T5JC>
2023-04-14 20:54:41 +0800

    Fix IDX Module

--- Exchanges/Idx.py
+++ Exchanges/Idx.py
@@ -1,69 +1,107 @@
 import pandas as pd
-import json
 from Models.Asset import Asset
-import requests
 import asyncio
 from aiohttp import ClientSession
-
+import math
+import http.client
+import json
+import datetime
 
 class Idx:
     def __init__(self):
-        self.all_symbols = json.load(open('Exchanges/idx_symbols.json'))
         self.all_asset_dataframe = None
-        #pd.options.display.float_format = '{:.0f}'.format
+
+    def convert_to_symbols(self, symbols):
+        return symbols
+
+    async def coro_scanner(self, session, page, param):
+        print("Page: ", page)
+        async with session.post(param['url'], headers=param['headers'], data=param['payload'].format(page)) as response:
+            response = await response.read()
+            if response is not None:
+                result = json.loads(response.decode('utf-8'))
+                return result
+
+    def get_from_screener(self, param):
+        all_hits = []
+        page = 1
+
+        conn = http.client.HTTPSConnection("www.investing.com")
+        payload = param['Filter'] + "&pn={0}&order[col]=viewData.symbol&order[dir]=a"
+        headers = {
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.56',
+            'x-requested-with': 'XMLHttpRequest'
+        }
+        conn.request("POST", "/stock-screener/Service/SearchStocks", payload.format(page), headers)
+        res = conn.getresponse()
+        data = res.read()
+        temp = data.decode("utf-8")
+
+        if temp is not None:
+            response = json.loads(temp)
+            total = int(response['totalCount'])
+            total_page = math.ceil(total / 50)
+            print("Total Hits: ", total)
+            print("Total Pages: ", total_page)
+            coro_result = self.loop_all_symbols(self.coro_scanner, list(range(1, total_page + 1)), {"payload": payload, "headers": headers, "url": "https://www.investing.com/stock-screener/Service/SearchStocks"})
+
+            for h in coro_result:
+                if 'hits' in h:
+                    all_hits.extend(h['hits'])
+                else:
+                    print('HITS NOT FOUND')
+
+        result = [a["viewData"]["symbol"] for a in all_hits]
+
+        return result
+
+    def cleanup_fund_symbol(self, input):
+        return input.split("_")[0]
 
     def get_all_dataframe(self, param):
         result = []
 
         if param.__contains__('Symbols') and isinstance(param['Symbols'], list) and len(param['Symbols']) > 0:
-            allSymbols = param['Symbols']
-        else:
-            allSymbols = self.all_symbols['symbols']
-
-        limit_count = param['CountLimit']
-
-        interval_map = {
-            '1d': 'I',
-            '1w': 'J',
-            '1m': 'M'
-        }
-        param['Interval'] = interval_map[param['Interval']]
+            allSymbols = self.convert_to_symbols(param['Symbols'])
+        elif param.__contains__('Filter') and param['Filter'] is not None:
+            allSymbols = self.get_from_screener(param)
 
         allKlines = self.loop_all_symbols(self.get_symbol_prices, allSymbols, param)
 
-        allKlines_json = []
-        for data in allKlines:
-            allKlines_json.append(json.loads(data.decode('utf-8')))
+        for klines in allKlines:
+            klineSymbol = klines[0]
+            response = klines[1]
 
-        for klines in allKlines_json:
-            if klines:
-                result_set = klines['B'].split('&')
-                df = pd.DataFrame([sub.split("|") for sub in result_set],
-                                  columns=["date", "empty1", "open", "high", "low", "close",
-                                           "dummy1", "dummy2", "zero", "vol", "ignore", "empty2"])
+            if (response is not None):
+                if(response['s'] != 'ok'):
+                    print("Error with response for " + klineSymbol)
+                else:
+                    response.pop('s')
+                    df = pd.DataFrame(response)
 
-                df.drop(df[df.date == ''].index, inplace=True)
+                    #remove row where all columns are null
+                    df = df[df.o.notna() & df.h.notna() & df.l.notna() & df.c.notna() & df.v.notna()]
 
-                if param['Interval'] == 'M':
-                    df.date = ["{0}01".format(d[:-2]) for d in df.date]
+                    if df.isnull().values.any():
+                        df = df.fillna(0)
 
-                df = df.tail(limit_count)
-                df = df.reset_index()           # !!! IMPORTANT, otherwise 2nd axes will start from the middle
-                df['index'] = df.index
+                    df.columns = ["date", "close", "open", "high", "low", "vol"]
+                    df = df.reset_index()
+                    df['index'] = df.index
 
-                # formatting column
-                df.date = pd.to_datetime(df.date, format='%Y%m%d')
-                df.open = df.open.astype(float)
-                df.high = df.high.astype(float)
-                df.low = df.low.astype(float)
-                df.close = df.close.astype(float)
-                df.vol = df.vol.astype(float)
+                    # formatting column
+                    df.date = pd.to_datetime(df.date, unit='ms')
+                    df.open = df.open.astype(float)
+                    df.high = df.high.astype(float)
+                    df.low = df.low.astype(float)
+                    df.close = df.close.astype(float)
+                    df.vol = df.vol.astype(float)
 
-                df['is_up'] = df.open - df.close <= 0
+                    df['is_up'] = df.open - df.close <= 0
 
-                #df.set_index('date', inplace=True)
-                asset = Asset(klines['A'].replace('1', ''), df, param['Interval'])
-                result.append(asset)
+                    asset = Asset(klineSymbol, df, param['Resolution'])
+                    result.append(asset)
 
         self.all_asset_dataframe = result
         return result
@@ -71,11 +109,24 @@ class Idx:
     def get_all_depths(self, bottom_limit):
         pass
 
+    def get_key(self, input, param):
+        result = "STOCK"
+        result += "/" + param["CountryMapping"][input["country"]]
+        result += "/" + param["ExchangeMapping"][input["exchange"]]
+        result += "/" + input["symbol"]
+        return result
+
     async def get_symbol_prices(self, session, symbol, param):
         print(symbol)
-        url = "https://hots.miraeasset.co.id/tr/cpstChartAjaxTR.do?StockCode={0}&periodBit={1}".format(symbol, param['Interval'])
+
+        start_date = int(datetime.datetime.strptime(param["StartDate"], '%d%m%Y').timestamp())
+        to_date = int(datetime.datetime.now().timestamp())
+        url = (f"https://chart-cloud.poems.co.id/iface/history?symbol={symbol}&resolution={param['Resolution']}&from={start_date}&to={to_date}")
         async with session.get(url) as response:
-            return await response.read()
+            response = await response.read()
+            if response is not None:
+                candles = json.loads(response.decode('utf-8'))
+                return symbol, candles
 
     async def bound_fetch(self, session, sem, coro, symbol, param):
         async with sem:
@@ -83,9 +134,12 @@ class Idx:
 
     async def run(self, coro, all_symbols, param):
         tasks = []
-        sem = asyncio.Semaphore(60)
+        sem = asyncio.Semaphore(1000)
 
-        async with ClientSession() as session:
+        headers = {
+            'Cookie': '_gcl_au=1.1.726795822.1681469770; _ga=GA1.3.290348481.1681469771; _gid=GA1.3.169319800.1681469771; _fbp=fb.2.1681469770981.1187568877; __zlcmid=1FNlkJx6uljViw4; POEMSChartCookie=s%3AVQiMsvJ5ocgSi4QrKBO003idzLIaFTaT.dEgz%2FyApNiRQraHpVf5hEA142OvKa2JC%2FXMrypImSAo'
+        }
+        async with ClientSession(headers=headers) as session:
             for symbol in all_symbols:
                 # pass Semaphore and session to every GET request
                 task = asyncio.ensure_future(self.bound_fetch(session, sem, coro, symbol, param))
